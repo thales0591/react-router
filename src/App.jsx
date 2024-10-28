a171de9e8ea801f51fd95d8b158d71d4f22d40e5
thales0591 <thales0591@gmail.com>
2024-10-28 18:34:50 -0300

    feat: add link component on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Home from "./pages/Home";
 import About from "./pages/About";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
     <div className='App'>
       <h1>React Router</h1>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/about' element={<About />}/>
