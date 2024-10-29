17c5adb7749d2a3353c06cc9f4b56bc64f327740
thales0591 <thales0591@gmail.com>
2024-10-29 10:20:42 -0300

    feat: create a search resource with useSearchParams hook

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Navbar from './components/Navbar';
 import Product from './pages/Product';
 import Info from './pages/Info';
 import NotFound from './pages/NotFound';
+import SearchForm from './components/SearchForm';
+import Search from './pages/Search';
 
 function App() {
   
@@ -15,11 +17,13 @@ function App() {
       <h1>React Router</h1>
       <BrowserRouter>
         <Navbar />
+        <SearchForm/>
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/about' element={<About />}/>
           <Route path='/products/:id' element={<Product/>}/>
           <Route path='/products/:id/info' element={<Info/>}/>
+          <Route path='/search' element={<Search/>}/>
           <Route path='*' element={<NotFound/>}/>
         </Routes>
       </BrowserRouter>
