4aaeda3a4e1197e8377e6f2896da9fc746f6fb64
thales0591 <thales0591@gmail.com>
2024-10-29 08:32:40 -0300

    feat: add navlink in place of link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import "./Navbar.css"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>Sobre</Link>
+        {/* <Link to='/'>Home</Link>
+        <Link to='/about'>Sobre</Link> */}
+        <NavLink to='/'>Home</NavLink>
+        {/*className={({isActive}) => (isActive ? 'esta-ativo' : 'nao-ativo')}*/}
+        <NavLink to='/about'>Sobre</NavLink>
     </nav>
   )
 }
