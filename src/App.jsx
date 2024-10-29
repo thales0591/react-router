9d6355ee77c705a3c40e0608ec0d9178087789d5
thales0591 <thales0591@gmail.com>
2024-10-29 08:20:02 -0300

    feat: add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Product from './pages/Product';
 import Info from './pages/Info';
+import NotFound from './pages/NotFound';
 
 function App() {
   
@@ -19,6 +20,7 @@ function App() {
           <Route path='/about' element={<About />}/>
           <Route path='/products/:id' element={<Product/>}/>
           <Route path='/products/:id/info' element={<Info/>}/>
+          <Route path='*' element={<NotFound/>}/>
         </Routes>
       </BrowserRouter>
     </div>
